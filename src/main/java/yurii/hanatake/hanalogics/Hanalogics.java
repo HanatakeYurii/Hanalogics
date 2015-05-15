8abc84706f5653e7543a9ac76845ce12498b2eee
HanatakeYurii <empirebd1@hotmail.com>
2015-05-15 03:26:16 +0200

    base mod files

--- src/main/java/yurii/hanatake/hanalogics/Hanalogics.java
+++ src/main/java/yurii/hanatake/hanalogics/Hanalogics.java
@@ -1,10 +1,32 @@
 package yurii.hanatake.hanalogics;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod (modid=Properties.MODID, name = Properties.MODNAME, version = Properties.MODVERSION, acceptedMinecraftVersions = Properties.MCVERSIONS, dependencies = Properties.DEPENDENCIES)
 public class Hanalogics {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@Mod.Instance(Properties.MODID)
+	public static Hanalogics instance;
+	
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		//Configs, blocks & items registering
 	}
+	
+	@Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	//GUI, crafting recipes, tile entities
+    }
+    
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    	//huh...
+    }
 
 }
---
+++ src/main/java/yurii/hanatake/hanalogics/Properties.java
@@ -0,0 +1,14 @@
+package yurii.hanatake.hanalogics;
+
+public class Properties {
+	//Mod infos
+	public static final String MODID = "hanalogics";
+    public static final String MODNAME = "Hanalogics";
+    static final String modver = "0.1";
+    static final String formc = "1.7.10";
+    static final String forge = "10.13.2.1291";
+    
+    public static final String MODVERSION = formc + "-" + modver;
+    public static final String MCVERSIONS = "[" + formc + ",)";
+    public static final String DEPENDENCIES = "required-after:Forge@[" + forge + ",)";
+}
